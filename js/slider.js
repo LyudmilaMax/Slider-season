9c5eb387aa6c2a07a4213e839bae767bad6f4247
Lyudmila Maximenko <109598383+LyudmilaMax@users.noreply.github.com>
2022-09-18 00:25:53 +0300

    add

--- js/slider.js
+++ js/slider.js
@@ -1,42 +1,8 @@
-// $('.center').slick({
-//   centerMode: true,
-//   centerPadding: '60px',
-//   slidesToShow: 3,
-//   responsive: [
-//     {
-//       breakpoint: 768,
-//       settings: {
-//         arrows: false,
-//         centerMode: true,
-//         centerPadding: '40px',
-//         slidesToShow: 3
-//       }
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         arrows: false,
-//         centerMode: true,
-//         centerPadding: '40px',
-//         slidesToShow: 1
-//       }
-//     }
-//   ]
-// });
-
-// $('.autoplay').slick({
-//   slidesToShow: 3,
-//   slidesToScroll: 1,
-//   autoplay: true,
-//   autoplaySpeed: 2000,
-// });
-
-$('.variable-width').slick({
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
   dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  centerMode: true,
-  variableWidth: true
+  autoplay: true,
+  autoplaySpeed: 2000,
+  
 });
-				
